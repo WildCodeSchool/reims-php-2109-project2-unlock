4c9790afb19242fe7260aeaee795c420900d0c1c
Cyl <labetowiez@aol.fr>
2021-10-26 16:50:32 +0200

    implement routes for the card list

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -4,5 +4,8 @@ namespace App\Controller;
 
 class CardController extends AbstractController
 {
-
+    public function list()
+    {
+        return $this->twig->render("Card/list.html.twig");
+    }
 }
