cf6209741e740222ecd90fb423c5e6b41b8ad7b7
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2021-10-26 17:02:35 +0200

    added route list and function list

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -4,5 +4,8 @@ namespace App\Controller;
 
 class GameController extends AbstractController
 {
-
+    public function list()
+    {
+        return $this->twig->render('Home/index.html.twig');
+    }
 }
