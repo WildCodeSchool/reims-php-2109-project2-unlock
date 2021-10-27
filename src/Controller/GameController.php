fada6760cc70f4ae503500a307b96236d225920d
Marionptnt <dev.marionpatinet@gmail.com>
2021-10-27 10:41:06 +0200

    add game description

--- src/Controller/GameController.php
+++ src/Controller/GameController.php
@@ -2,7 +2,19 @@
 
 namespace App\Controller;
 
+use App\Model\GameManager;
+
 class GameController extends AbstractController
 {
 
+    /**
+    * Show informations for a specific game
+    */
+    public function show(int $id): string
+    {
+        $gameManager = new GameManager();
+        $game = $gameManager->selectOneById($id);
+
+        return $this->twig->render('Game/show.html.twig', ['game' => $game]);
+    }
 }
--- src/routes.php
+++ src/routes.php
@@ -10,7 +10,7 @@ return [
     '' => ['HomeController', 'index',],
     'items' => ['ItemController', 'index',],
     'items/edit' => ['ItemController', 'edit', ['id']],
-    'items/show' => ['ItemController', 'show', ['id']],
+    'game/show' => ['GameController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
 ];
