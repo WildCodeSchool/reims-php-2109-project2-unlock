1de7764b7d339e70dff77ed6fcb081f4d0e06a64
Sousa <luis.sousa89360@gmail.com>
2021-10-27 10:56:34 +0200

    added card creation

--- src/Controller/CardController.php
+++ src/Controller/CardController.php
@@ -2,7 +2,25 @@
 
 namespace App\Controller;
 
+use App\Model\CardManager;
+
 class CardController extends AbstractController
 {
+    public function add(): string
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            // clean $_POST data
+            $card = array_map('trim', $_POST);
+
+            // TODO validations (length, format...)
+
+            // if validation is ok, insert and redirection
+            $cardManager = new cardManager();
+            $id = $cardManager->insert($card);
+            header('Location:/cards/show?id=' . $id);
+        }
+
+        return $this->twig->render('Card/add.html.twig');
+    }
 
 }
