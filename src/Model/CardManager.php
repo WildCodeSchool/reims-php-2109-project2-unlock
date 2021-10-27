2c7bf1d3a482a479efd5b7ca51236850c011b2d5
Sousa <luis.sousa89360@gmail.com>
2021-10-27 11:25:34 +0200

    added a space

--- src/Model/CardManager.php
+++ src/Model/CardManager.php
@@ -10,7 +10,7 @@ class CardManager extends AbstractManager
     public function insert(array $card): int
     {
         $statement = $this->pdo->prepare(
-          "INSERT INTO " . self::TABLE . " (`name`, `description`) VALUES (:name, :description)"
+            "INSERT INTO " . self::TABLE . " (`name`, `description`) VALUES (:name, :description)"
       );
         $statement->bindValue('name', $card['name'], \PDO::PARAM_STR);
         $statement->bindValue('description', $card['description'], \PDO::PARAM_STR);
