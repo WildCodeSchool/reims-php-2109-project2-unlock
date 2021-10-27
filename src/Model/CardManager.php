1a5e2256ad2cd4a07d27ef669ed34e9136e2d9ff
Luis Sousa <89387539+SousaLuis@users.noreply.github.com>
2021-10-27 11:19:01 +0200

    Update CardManager.php
    added space

--- src/Model/CardManager.php
+++ src/Model/CardManager.php
@@ -5,13 +5,13 @@ namespace App\Model;
 class CardManager extends AbstractManager
 {
 
-  public const TABLE = "card";
+    public const TABLE = "card";
 
     public function insert(array $card): int
     {
-      $statement = $this->pdo->prepare(
-        "INSERT INTO " . self::TABLE . " (`name`, `description`) VALUES (:name, :description)"
-        );
+        $statement = $this->pdo->prepare(
+          "INSERT INTO " . self::TABLE . " (`name`, `description`) VALUES (:name, :description)"
+      );
         $statement->bindValue('name', $card['name'], \PDO::PARAM_STR);
         $statement->bindValue('description', $card['description'], \PDO::PARAM_STR);
 
