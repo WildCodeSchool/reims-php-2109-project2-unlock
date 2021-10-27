4e4e076fde38aaeb9210e53320404f5ca9e5b7b3
Cyl <labetowiez@aol.fr>
2021-10-27 10:31:56 +0200

    change route path to "cards"

--- src/routes.php
+++ src/routes.php
@@ -13,5 +13,5 @@ return [
     'items/show' => ['ItemController', 'show', ['id']],
     'items/add' => ['ItemController', 'add',],
     'items/delete' => ['ItemController', 'delete',],
-    'card/list' => ['CardController', 'list',],
+    'cards' => ['CardController', 'list',],
 ];
