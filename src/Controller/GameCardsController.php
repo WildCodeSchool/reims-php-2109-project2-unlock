0f429c7b933537c9a2ffc2c8b5e79e37b5bff6c8
Cyl <labetowiez@aol.fr>
2021-10-27 17:17:41 +0200

    selected cards go to back end

--- src/Controller/GameCardsController.php
+++ src/Controller/GameCardsController.php
@@ -2,10 +2,19 @@
 
 namespace App\Controller;
 
+use App\Model\CardManager;
+
 class GameCardsController extends AbstractController
 {
     public function add(/*int $id*/)
     {
-        return $this->twig->render("GameCards/cardsForm.html.twig");
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $cards = array_map('trim', $_POST);
+            var_dump($cards);
+        }
+
+        $cardManager = new CardManager();
+        $cards = $cardManager->selectAll();
+        return $this->twig->render("GameCards/cardsForm.html.twig", ["cards" => $cards]);
     }
 }
