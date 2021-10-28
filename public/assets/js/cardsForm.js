7e99361a8aad06be5ee80b9b128c72691fde6d08
Cyl <labetowiez@aol.fr>
2021-10-28 13:49:18 +0200

    js working

--- public/assets/js/cardsForm.js
+++ public/assets/js/cardsForm.js
@@ -1,1 +1,9 @@
-console.log("hello there");
+cards = document.getElementsByClassName("card");
+
+for (let i in cards) {
+    cards[i].onmousedown = function() {
+        checkbox = cards[i].getElementsByTagName("input")[0];
+        checkbox.checked ? checkbox.checked = false : checkbox.checked = true;
+        checkbox.checked ? cards[i].classList.add("checked") : cards[i].classList.remove("checked");
+    };
+}
