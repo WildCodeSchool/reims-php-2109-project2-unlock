ec955dcd1623f34dfc4552f4fba3471d4636276d
Cyl <labetowiez@aol.fr>
2021-10-28 14:26:11 +0200

    Hide checkbox, and add border properly

--- public/assets/js/cardsForm.js
+++ public/assets/js/cardsForm.js
@@ -1,6 +1,7 @@
 cards = document.getElementsByClassName("card");
 
 for (let i in cards) {
+    cards[i].getElementsByTagName("input")[0].checked = false;
     cards[i].onmousedown = function() {
         checkbox = cards[i].getElementsByTagName("input")[0];
         checkbox.checked ? checkbox.checked = false : checkbox.checked = true;
