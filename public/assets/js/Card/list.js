1a5df92d8238612fb01727d261188447ab013b50
Cyl <labetowiez@aol.fr>
2021-11-01 15:48:15 +0100

    js working properly

--- public/assets/js/Card/list.js
+++ public/assets/js/Card/list.js
@@ -1,1 +1,15 @@
-console.log("hello there");
+cards = document.getElementsByClassName("card");
+option = document.getElementById("option");
+listOption = document.getElementById("list-option");
+
+option.addEventListener("input", function() {
+    for (index in cards) {
+        cards[index].classList.toggle("list-style");
+    }
+});
+
+if (listOption.checked) {
+    for (index in cards) {
+        cards[index].classList.add("list-style");
+    }
+}
