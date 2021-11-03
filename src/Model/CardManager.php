8aec351a638e91eb36a17180a9331bcf393a308e
TarikElha <www.elhantiti.t@gmail.com>
2021-11-03 15:33:10 +0100

    updated instruction field in card table and update css

--- src/Model/CardManager.php
+++ src/Model/CardManager.php
@@ -9,7 +9,7 @@ class CardManager extends AbstractManager
 
     public function insert(array $card): int
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `description`, `instruction`, `image`) VALUES (:name, :description, :instruction, :image)");
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `description`, `instruction`, `image`) VALUES (:name, :description, :instruction, :image)");// phpcs:ignore
         $statement->bindValue('name', $card['name'], \PDO::PARAM_STR);
         $statement->bindValue('description', $card['description'], \PDO::PARAM_STR);
         $statement->bindValue('instruction', $card['instruction'], \PDO::PARAM_STR);
