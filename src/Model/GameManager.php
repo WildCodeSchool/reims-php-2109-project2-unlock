6c6881b1e400f884cf9db5800cbc168ea339b095
Xavier CHAVAUDREY <chavaudreyxav@hotmail.fr>
2021-11-03 17:15:07 +0100

    create-instruction-game-in-add

--- src/Model/GameManager.php
+++ src/Model/GameManager.php
@@ -9,10 +9,11 @@ class GameManager extends AbstractManager
     //name, description, image url
     public function insert(array $game): int
     {
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `description`)
-         VALUES (:name, :description)");
+        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`name`, `description`, `instruction`)
+         VALUES (:name, :description, :instruction)");
         $statement->bindValue('name', $game['name'], \PDO::PARAM_STR);
         $statement->bindValue('description', $game['description'], \PDO::PARAM_STR);
+        $statement->bindValue('instruction', $game['instruction'], \PDO::PARAM_STR);
 
         $statement->execute();
         return (int)$this->pdo->lastInsertId();
