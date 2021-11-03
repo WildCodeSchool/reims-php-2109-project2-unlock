ccfa23db6aaed27f5bd436165dca11f3d8634742
TarikElha <www.elhantiti.t@gmail.com>
2021-11-03 15:32:45 +0100

    updated instruction field in card table and update css

--- populate.sql
+++ populate.sql
@@ -6,14 +6,14 @@ INSERT INTO `game` (`name`, `description`) VALUES
 ("Sherlock dans le égout de londre", "Une aventure nauséabonde et visieuse vous attend"),
 ("Shreklock Holmes, le retour de Farquaad", "Get out of my swamp !");
 
-INSERT INTO `card` (`name`, `description`) VALUES 
-("Sherlock house", "You wake up in your living room, a little bit drunk. But where are youor house key !?!"),
-("Key", "Here is a key to open door"),
-("Door", "Here is a door. Find a key to open it"),
-("Sword", "You can probably kill someone with it, be carefull"),
-("Handkerchief", "Probably usefull, KEEP IT"),
-("Smoking pipe", "Probably your best friend"),
-("Bookshelf", "You can clearly see some inscription on it"),
-("Book", "Such a great story, reminds me when i was young"),
-("Horse", "Look at my Horse, my horse is amazing!"),
-("Phone", "You can phone to your best friend with it, can you?");
+INSERT INTO `card` (`name`, `description`, `instruction`, `image`) VALUES 
+("Sherlock house", "You wake up in your living room, a little bit drunk. But where are your house key !?!", "Check the cards that you can see over the picture", "/assets/images/start.webp"),
+("Key", "Here is a key to open door", "", ""),
+("Door", "Here is a door. Find a key to open it", "", ""),
+("Sword", "You can probably kill someone with it, be carefull", "", ""),
+("Handkerchief", "Probably usefull, KEEP IT", "", ""),
+("Smoking pipe", "Probably your best friend", "", ""),
+("Bookshelf", "You can clearly see some inscription on it", "", ""),
+("Book", "Such a great story, reminds me when i was young", "", ""),
+("Horse", "Look at my Horse, my horse is amazing!", "", ""),
+("Phone", "You can phone to your best friend with it, can you?", "", "");
