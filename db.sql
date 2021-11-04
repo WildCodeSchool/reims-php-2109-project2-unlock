28c649bfa524d0d2b55101de0e522d82709fff52
Cyl <labetowiez@aol.fr>
2021-11-04 15:23:30 +0100

    i shouldn't do this

--- db.sql
+++ db.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS `card` (
   `id` INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `name` VARCHAR(100),
   `description` TEXT,
-  `type` VARCHAR(100)
+  `image_url` VARCHAR(100)
 ) ENGINE = InnoDB CHARACTER SET latin1 COLLATE latin1_bin ;
 
 CREATE TABLE IF NOT EXISTS `game` (
--- populate.sql
+++ populate.sql
@@ -6,22 +6,26 @@ INSERT INTO `game` (`name`, `description`, `instruction`) VALUES
 ("Sherlock dans le égout de londre", "Une aventure nauséabonde et visieuse vous attend","A l'aide des différentes combinaisons de cartes, recherche la clé qui te mènera vers la sortie."),
 ("Shreklock Holmes, le retour de Farquaad", "Get out of my swamp !","A l'aide des différentes combinaisons de cartes, recherche la clé qui te mènera vers la sortie.");
 
-INSERT INTO `card` (`name`, `description`) VALUES
-("Key", "Here is a key to open door"),
-("Door", "Here is a door. Find a key to open it"),
-("Smoking pipe", "Probably your best friend"),
-("Bookshelf", "You can clearly see some inscription on it"),
-("Book", "Such a great story, reminds me when i was young"),
-("Horse", "Look at my Horse, my horse is amazing!"),
-("Phone", "You can phone to your best friend with it, can you?"),
-("Sword", "You can probably kill someone with it, be carefull"),
-("Handkerchief", "Probably usefull, KEEP IT"),
-("Gobelin", "Kill this gobelin"),
-("Finished", "you finished the game");
+INSERT INTO `card` (`name`, `description`, `image_url`) VALUES
+("Salon", "Voici la pièce où vous êtes enfermés. Plusieurs élements sont visibles. Vous pouvez maintenant associer les cartes qui vous mêneront vers la sortie.", ""),
+("Cable", "Voici un cable téléphonique.", "/assets/images/cable.png"),
+("Clé", "C'est une clé, qui permet sûrement d'ouvrir une serrure...", "/assets/images/key.png"),
+("Armoire", "Une armoir fermée à clé.", "/assets/images/armoire.png"),
+("Watson", "Il vous connait bien, il peut peut-être vous aider.", "/assets/images/watson.png"),
+("Porte", "La porte d'entrée est fermée à clé.", "/assets/images/door.png"),
+("Carnet téléphonique", "Ce carnet contient le numéro de téléphone de Watson.", "/assets/images/carnet.png"),
+("Téléphone", "Appel à un ami.", "/assets/images/phone.png"),
+("Livre", "Ce livre contient une clé.", "/assets/images/book.png"),
+("Fil de Téléphone", "Brancher votre téléphone!", "/assets/images/fil.png"),
+("Fin", "", "");
 
 INSERT INTO `gamecards` (`game_id`, `card_id`, `card_number`, `available_on_begin`) VALUES
-(1, 1, 3, 1),
-(1, 2, 5, 1),
-(1, 3, 8, 0),
-(1, 10, 10, 1),
-(1, 11, 18, 0);
+(1, 3, 2, 1),
+(1, 4, 1, 1),
+(1, 5, 11, 1),
+(1, 6, 29, 1),
+(1, 10, 4, 1),
+(1, 7, 3, 0),
+(1, 8, 7, 0),
+(1, 9, 18, 0),
+(1, 11, 47, 0);
